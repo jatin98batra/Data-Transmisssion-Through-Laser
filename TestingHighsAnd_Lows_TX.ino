ce55cca01b14bab26cf5e7f5bd7e2432f947f18c
jatinkeshav24 <37905200+jatinkeshav24@users.noreply.github.com>
2018-12-01 21:49:34 +0530

    This is the file that was used to sense the high and low signal to register the logical HIGH and LOW respectively
    This is the actual logic behind the source code , this is not the source code  , this was just used for testing .

---
+++ TestingHighsAnd_Lows_TX.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10,OUTPUT);
+  digitalWrite(10,HIGH);
+  pinMode(4,INPUT_PULLUP);
+  Serial.begin(9600);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(10,HIGH);
+while(digitalRead(4)== HIGH); //hold !
+delay(1000);
+digitalWrite(10,LOW);
+Serial.println("yo");
+delay(97);
+digitalWrite(10,HIGH);
+
+
+}
---
+++ TestingHighsAnd_Lows_rx.ino
@@ -0,0 +1,17 @@
+int stop_t,start;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,INPUT_PULLUP);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(digitalRead(8)== HIGH);
+  start=millis();
+  while(digitalRead(8)== LOW);
+  stop_t=millis();
+
+  Serial.write(stop_t-start);
+
+}
